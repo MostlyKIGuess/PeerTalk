9509614c14907f41167eb9a8196e41ea56945d8e
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2024-10-26 20:40:01 +0530

    [+]Chat

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Ripple from "@/components/ui/ripple";
+import Chat from "@/components/chat";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       </p>
       <Ripple />
     </div>
+    <Chat />
 
     </>
   );
---
+++ frontend/src/components/chat.tsx
@@ -0,0 +1,250 @@
+"use client";
+
+import {
+  ChatBubble,
+  ChatBubbleAction,
+  ChatBubbleAvatar,
+  ChatBubbleMessage,
+} from "@/components/ui/chat/chat-bubble";
+import { ChatInput } from "@/components/ui/chat/chat-input";
+import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
+import { Button } from "@/components/ui/button";
+import {
+  CopyIcon,
+  CornerDownLeft,
+  Paperclip,
+  RefreshCcw,
+  Volume2,
+} from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import CodeDisplayBlock from "@/components/code-display-block";
+import { HfInference } from "@huggingface/inference";
+
+const hf = new HfInference(process.env.NEXT_PUBLIC_HF_API_KEY);
+
+const ChatAiIcons = [
+  {
+    icon: CopyIcon,
+    label: "Copy",
+  },
+  {
+    icon: RefreshCcw,
+    label: "Refresh",
+  },
+  {
+    icon: Volume2,
+    label: "Volume",
+  },
+];
+
+interface Message {
+  role: "user" | "assistant";
+  content: string;
+}
+
+export default function Home() {
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const messagesRef = useRef<HTMLDivElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+
+    const userMessage: Message = { role: "user", content: input };
+    setMessages([...messages, userMessage]);
+    setInput("");
+    setIsGenerating(true);
+
+    try {
+    //   const response = await hf.textGeneration({
+    //     model: "zementalist/llama-3-8B-chat-psychotherapist",
+    //     inputs: input,
+    //   });
+      const response = await hf.textGeneration({
+        model: "gpt2",
+        inputs: input,
+      });
+
+      const aiMessage: Message = { role: "assistant", content: response.generated_text };
+      setMessages((prevMessages) => [...prevMessages, aiMessage]);
+    } catch (error) {
+      console.error("Error generating response:", error);
+    } finally {
+      setIsGenerating(false);
+    }
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      if (isGenerating || isLoading || !input) return;
+      setIsGenerating(true);
+      handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+    }
+  };
+
+  const handleActionClick = async (action: string, messageIndex: number) => {
+    console.log("Action clicked:", action, "Message index:", messageIndex);
+    if (action === "Refresh") {
+      setIsGenerating(true);
+      try {
+        const message = messages[messageIndex];
+        // const response = await hf.textGeneration({
+        //   model: "zementalist/llama-3-8B-chat-psychotherapist",
+        //   inputs: message.content,
+        // });
+         const response = await hf.textGeneration({
+          model: "gpt2",
+          inputs: message.content,
+        });
+
+        const aiMessage: Message = { role: "assistant", content: response.generated_text };
+        setMessages((prevMessages) => {
+          const newMessages = [...prevMessages];
+          newMessages[messageIndex] = aiMessage;
+          return newMessages;
+        });
+      } catch (error) {
+        console.error("Error reloading:", error);
+      } finally {
+        setIsGenerating(false);
+      }
+    }
+
+    if (action === "Copy") {
+      const message = messages[messageIndex];
+      if (message && message.role === "assistant") {
+        navigator.clipboard.writeText(message.content);
+      }
+    }
+  };
+
+  return (
+    <main className="flex h-screen w-full max-w-3xl flex-col items-center mx-auto py-6">
+      <ChatMessageList ref={messagesRef}>
+        {/* Initial Message */}
+        {messages.length === 0 && (
+          <div className="w-full bg-background shadow-sm border rounded-lg p-8 flex flex-col gap-2">
+            <h1 className="font-bold">Welcome to PeerTalk.</h1>
+            <p className="text-muted-foreground text-sm">
+              Talk with your AI assistant about anything you want. Just start
+            </p>
+          </div>
+        )}
+
+        {/* Messages */}
+        {messages &&
+          messages.map((message, index) => (
+            <ChatBubble
+              key={index}
+              variant={message.role == "user" ? "sent" : "received"}
+            >
+              <ChatBubbleAvatar
+                src=""
+                fallback={message.role == "user" ? "👨🏽" : "🤖"}
+              />
+              <ChatBubbleMessage>
+                {message.content
+                  .split("```")
+                  .map((part: string, index: number) => {
+                    if (index % 2 === 0) {
+                      return (
+                        <Markdown key={index} remarkPlugins={[remarkGfm]}>
+                          {part}
+                        </Markdown>
+                      );
+                    } else {
+                      return (
+                        <pre className="whitespace-pre-wrap pt-2" key={index}>
+                          <CodeDisplayBlock code={part} lang="" />
+                        </pre>
+                      );
+                    }
+                  })}
+
+                {message.role === "assistant" &&
+                  messages.length - 1 === index && (
+                    <div className="flex items-center mt-1.5 gap-1">
+                      {!isGenerating && (
+                        <>
+                          {ChatAiIcons.map((icon, iconIndex) => {
+                            const Icon = icon.icon;
+                            return (
+                              <ChatBubbleAction
+                                variant="outline"
+                                className="size-5"
+                                key={iconIndex}
+                                icon={<Icon className="size-3" />}
+                                onClick={() =>
+                                  handleActionClick(icon.label, index)
+                                }
+                              />
+                            );
+                          })}
+                        </>
+                      )}
+                    </div>
+                  )}
+              </ChatBubbleMessage>
+            </ChatBubble>
+          ))}
+
+        {/* Loading */}
+        {isGenerating && (
+          <ChatBubble variant="received">
+            <ChatBubbleAvatar src="" fallback="🤖" />
+            <ChatBubbleMessage isLoading />
+          </ChatBubble>
+        )}
+      </ChatMessageList>
+      <div className="w-full px-4">
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="relative rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
+        >
+          <ChatInput
+            value={input}
+            onKeyDown={onKeyDown}
+            onChange={handleInputChange}
+            placeholder="Type your message here..."
+            className="min-h-12 resize-none rounded-lg bg-background border-0 p-3 shadow-none focus-visible:ring-0"
+          />
+          <div className="flex items-center p-3 pt-0">
+            <Button variant="ghost" size="icon">
+              <Paperclip className="size-4" />
+              <span className="sr-only">Attach file</span>
+            </Button>
+
+            <Button
+              disabled={!input || isLoading}
+              type="submit"
+              size="sm"
+              className="ml-auto gap-1.5"
+            >
+              Send Message
+              <CornerDownLeft className="size-3.5" />
+            </Button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
