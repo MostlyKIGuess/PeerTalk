3829ee0d07b8124051ad82e74afa327a2ce12505
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2024-10-30 23:00:10 +0530

    [+]nice user data

--- frontend/src/components/chat.tsx
+++ frontend/src/components/chat.tsx
@@ -61,9 +61,11 @@ export default function Home() {
     fetch(`${backendUrl}/api/session/start`, {
       method: "GET",
       headers: {
+        
         "Content-Type": "application/json",
+      
       },
-    })
+})
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
